c15670c30141134d08898368a8cef0e73649a2a4
1shabarinathreddy <thirumalashabarinath@gmail.com>
2024-12-01 01:48:35 +0000

    added users table

--- carD/app.py
+++ carD/app.py
@@ -1,18 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
-db = SQLAlchemy(app)
-
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'dynamic postgres db name'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://masthan:Masthan123@localhost:5432/carsdb'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
+# User Model
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    phone = db.Column(db.String(20), nullable=False)
+    password = db.Column(db.String(200), nullable=False)
 
-
-@app.route('/')
-def home():
-    return render_template('index.html', message="Welcome to Flask!")
-
-# Run the app
 if __name__ == '__main__':
     app.run(debug=True)
